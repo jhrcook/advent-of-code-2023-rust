ac77433ba7fb4b54ba1c0ce1c0b4789a040145cf
Joshua Cook <joshua_cook@vrtx.com>
2023-12-01 06:23:30 -0500

    feat: day 1 puzzle 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,18 +1,48 @@
 use crate::data::load;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::num::ParseIntError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq)]
-pub enum PuzzleError {
+pub enum PuzzleError<'a> {
     #[error("Failed parsing integer.")]
-    ParseIntError(#[from] std::num::ParseIntError),
+    ParseIntError(#[from] ParseIntError),
+    #[error("Could not locate digit: {}.", .0)]
+    NoDigit(&'a str),
 }
 
-pub fn puzzle_1(input_data: &str) -> Result<u32, PuzzleError> {
-    Ok(1)
+fn extract_digits(text: &str) -> Vec<&str> {
+    lazy_static! {
+        static ref DIGIT_REGEX: Regex = Regex::new(r"[0-9]").unwrap();
+    }
+    DIGIT_REGEX
+        .find_iter(text)
+        .map(|mat| mat.as_str())
+        .collect()
+}
+
+pub fn puzzle_1(input_data: &str) -> Result<isize, PuzzleError> {
+    let mut total = 0;
+    for line in input_data.trim().lines().map(|a| a.trim()) {
+        let digits_str = extract_digits(line);
+        let number: isize = match format!(
+            "{}{}",
+            digits_str.first().ok_or(PuzzleError::NoDigit(line))?,
+            digits_str.last().ok_or(PuzzleError::NoDigit(line))?
+        )
+        .parse()
+        {
+            Ok(x) => x,
+            Err(e) => return Err(PuzzleError::ParseIntError(e)),
+        };
+        total += number
+    }
+    Ok(total)
 }
 
 pub fn main(data_dir: &str) {
-    println!("Day 1: Calorie Counting");
+    println!("Day 1: Trebuchet?!");
     let data = load(data_dir, 1, None);
 
     // Puzzle 1.
@@ -21,7 +51,7 @@ pub fn main(data_dir: &str) {
         Ok(x) => println!(" Puzzle 1: {}", x),
         _ => panic!("No solution to puzzle 1."),
     }
-    assert_eq!(answer_1, Ok(68787));
+    // assert_eq!(answer_1, Ok(68787));
 
     // Puzzle 2.
     // let answer_2 = puzzle_2(&data);
@@ -36,15 +66,14 @@ pub fn main(data_dir: &str) {
 mod tests {
     use crate::solutions::day01::puzzle_1;
 
-    const EXAMPLE_INPUT: &str = "";
+    const EXAMPLE_INPUT: &str = "
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet";
 
     #[test]
     fn example_1_puzzle_1() {
-        assert_eq!(puzzle_1(self::EXAMPLE_INPUT), Ok(1));
+        assert_eq!(puzzle_1(self::EXAMPLE_INPUT), Ok(142));
     }
-
-    // #[test]
-    // fn example_1_puzzle_2() {
-    //     assert_eq!(puzzle_2(self::EXAMPLE_INPUT), Ok(1));
-    // }
 }
