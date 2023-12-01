55c90880e077f6cb08cbb6b27b3a211bb84440ee
Joshua Cook <joshua_cook@vrtx.com>
2023-12-01 08:53:57 -0500

    feat: day 1 puzzle 2

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,44 +1,86 @@
 use crate::data::load;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::num::ParseIntError;
+use std::{collections::HashMap, num::ParseIntError};
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq)]
-pub enum PuzzleError<'a> {
+pub enum PuzzleError {
     #[error("Failed parsing integer.")]
     ParseIntError(#[from] ParseIntError),
-    #[error("Could not locate digit: {}.", .0)]
-    NoDigit(&'a str),
+    #[error("Could not locate digit.")]
+    NoDigits,
 }
 
-fn extract_digits(text: &str) -> Vec<&str> {
+fn extract_digits_1(text: &str) -> Vec<isize> {
     lazy_static! {
         static ref DIGIT_REGEX: Regex = Regex::new(r"[0-9]").unwrap();
     }
     DIGIT_REGEX
         .find_iter(text)
-        .map(|mat| mat.as_str())
+        .map(|mat| mat.as_str().parse::<isize>().unwrap())
         .collect()
 }
 
-pub fn puzzle_1(input_data: &str) -> Result<isize, PuzzleError> {
-    let mut total = 0;
-    for line in input_data.trim().lines().map(|a| a.trim()) {
-        let digits_str = extract_digits(line);
-        let number: isize = match format!(
-            "{}{}",
-            digits_str.first().ok_or(PuzzleError::NoDigit(line))?,
-            digits_str.last().ok_or(PuzzleError::NoDigit(line))?
-        )
-        .parse()
-        {
-            Ok(x) => x,
-            Err(e) => return Err(PuzzleError::ParseIntError(e)),
-        };
-        total += number
+fn extract_digits_2(text: &str) -> Vec<isize> {
+    let regex_set = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    let mut digits: Vec<Option<isize>> = Vec::from_iter((0..text.len()).map(|_| None));
+    for i in 0..text.len() {
+        for (re, val) in regex_set.iter() {
+            if text[i..].starts_with(re) {
+                digits[i] = Some(*val);
+                break;
+            }
+        }
     }
+    digits.iter().flatten().cloned().collect::<Vec<_>>()
+}
+
+fn make_calibration_number(digits: &[isize]) -> Result<isize, PuzzleError> {
+    Ok(digits.first().ok_or(PuzzleError::NoDigits)? * 10
+        + digits.last().ok_or(PuzzleError::NoDigits)?)
+}
+
+fn calc_total_calibration(
+    input_data: &str,
+    extraction_func: fn(&str) -> Vec<isize>,
+) -> Result<isize, PuzzleError> {
+    let total = input_data
+        .trim()
+        .lines()
+        .map(|a| a.trim())
+        .map(|line| make_calibration_number(&extraction_func(line)))
+        .collect::<Result<Vec<isize>, PuzzleError>>()?
+        .iter()
+        .sum();
     Ok(total)
+}
+
+pub fn puzzle_1(input_data: &str) -> Result<isize, PuzzleError> {
+    calc_total_calibration(input_data, extract_digits_1)
+}
+
+pub fn puzzle_2(input_data: &str) -> Result<isize, PuzzleError> {
+    calc_total_calibration(input_data, extract_digits_2)
 }
 
 pub fn main(data_dir: &str) {
@@ -49,22 +91,22 @@ pub fn main(data_dir: &str) {
     let answer_1 = puzzle_1(&data);
     match answer_1 {
         Ok(x) => println!(" Puzzle 1: {}", x),
-        _ => panic!("No solution to puzzle 1."),
+        Err(e) => panic!("No solution to puzzle 1: {}.", e),
     }
-    // assert_eq!(answer_1, Ok(68787));
+    assert_eq!(answer_1, Ok(56042));
 
     // Puzzle 2.
-    // let answer_2 = puzzle_2(&data);
-    // match answer_2 {
-    //     Ok(x) => println!(" Puzzle 2: {}", x),
-    //     _ => panic!("No solution to puzzle 2."),
-    // }
-    // assert_eq!(answer_2, Ok(198041))
+    let answer_2 = puzzle_2(&data);
+    match answer_2 {
+        Ok(x) => println!(" Puzzle 2: {}", x),
+        Err(e) => panic!("No solution to puzzle 2: {}", e),
+    }
+    assert_eq!(answer_2, Ok(55358))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solutions::day01::puzzle_1;
+    use crate::solutions::day01::{puzzle_1, puzzle_2};
 
     const EXAMPLE_INPUT: &str = "
     1abc2
@@ -72,8 +114,23 @@ mod tests {
     a1b2c3d4e5f
     treb7uchet";
 
+    const EXAMPLE_INPUT_2: &str = "
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    ";
+
     #[test]
     fn example_1_puzzle_1() {
         assert_eq!(puzzle_1(self::EXAMPLE_INPUT), Ok(142));
+    }
+
+    #[test]
+    fn example_1_puzzle_2() {
+        assert_eq!(puzzle_2(self::EXAMPLE_INPUT_2), Ok(281));
     }
 }
