5e78ba441d1ac5c49ff3c10ce150b72ab277ab82
Josh Cook <joshuacook0023@gmail.com>
2023-12-06 07:15:53 -0500

    Day 6 puzzle 2

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -11,29 +11,29 @@ pub enum PuzzleErr<'a> {
 }
 
 struct Race {
-    duration: u32,
-    record_distance: u32,
+    duration: u64,
+    record_distance: u64,
 }
 
 impl Race {
-    fn n_ways_to_win(&self) -> u32 {
+    fn n_ways_to_win(&self) -> u64 {
         (1..self.duration)
             .filter(|t| self.record_distance < (t * (self.duration - t)))
             .collect::<Vec<_>>()
-            .len() as u32
+            .len() as u64
     }
 }
 
-fn _get_nums(line: &str) -> Result<Vec<u32>, PuzzleErr> {
+fn _get_nums(line: &str) -> Result<Vec<u64>, PuzzleErr> {
     line.split(':')
         .nth(1)
         .ok_or(PuzzleErr::LineParsingError(line))?
         .split_whitespace()
-        .map(|s| s.parse::<u32>().or(Err(PuzzleErr::LineParsingError(line))))
+        .map(|s| s.parse::<u64>().or(Err(PuzzleErr::LineParsingError(line))))
         .collect::<Result<_, _>>()
 }
 
-fn parse_data(input: &str) -> Result<Vec<Race>, PuzzleErr> {
+fn parse_data1(input: &str) -> Result<Vec<Race>, PuzzleErr> {
     let lines = input.trim().lines().collect::<Vec<_>>();
     if lines.len() != 2 {
         return Err(PuzzleErr::InputDataError("Not two lines in input."));
@@ -48,11 +48,35 @@ fn parse_data(input: &str) -> Result<Vec<Race>, PuzzleErr> {
         .collect())
 }
 
-pub fn puzzle_1(input: &str) -> Result<u32, PuzzleErr> {
-    Ok(parse_data(input)?
+fn _get_num(line: &str) -> Result<u64, PuzzleErr> {
+    line.split(':')
+        .nth(1)
+        .ok_or(PuzzleErr::LineParsingError(line))?
+        .replace(' ', "")
+        .parse::<u64>()
+        .or(Err(PuzzleErr::LineParsingError(line)))
+}
+
+fn parse_data2(input: &str) -> Result<Race, PuzzleErr> {
+    let lines = input.trim().lines().collect::<Vec<_>>();
+    if lines.len() != 2 {
+        return Err(PuzzleErr::InputDataError("Not two lines in input."));
+    }
+    Ok(Race {
+        duration: _get_num(lines[0])?,
+        record_distance: _get_num(lines[1])?,
+    })
+}
+
+pub fn puzzle_1(input: &str) -> Result<u64, PuzzleErr> {
+    Ok(parse_data1(input)?
         .iter()
         .map(|r| r.n_ways_to_win())
         .product())
+}
+
+pub fn puzzle_2(input: &str) -> Result<u64, PuzzleErr> {
+    Ok(parse_data2(input)?.n_ways_to_win())
 }
 
 pub fn main(data_dir: &str) {
@@ -65,13 +89,13 @@ pub fn main(data_dir: &str) {
         Ok(x) => println!(" Puzzle 1: {}", x),
         Err(e) => panic!("No solution to puzzle 1: {}.", e),
     }
-    // assert_eq!(answer_1, Ok(650599855));
+    assert_eq!(answer_1, Ok(771628));
 
     // Puzzle 2.
-    // let answer_2 = puzzle_2(&data);
-    // match answer_2 {
-    //     Ok(x) => println!(" Puzzle 2: {}", x),
-    //     Err(e) => panic!("No solution to puzzle 2: {}", e),
-    // }
-    // assert_eq!(answer_2, Ok(1240035))
+    let answer_2 = puzzle_2(&data);
+    match answer_2 {
+        Ok(x) => println!(" Puzzle 2: {}", x),
+        Err(e) => panic!("No solution to puzzle 2: {}", e),
+    }
+    assert_eq!(answer_2, Ok(27363861))
 }
--- tests/test_day06.rs
+++ tests/test_day06.rs
@@ -1,4 +1,4 @@
-use aoc_2023::solutions::day06::puzzle_1;
+use aoc_2023::solutions::day06::{puzzle_1, puzzle_2};
 
 const EXAMPLE_INPUT: &str = "
 Time:      7  15   30
@@ -10,7 +10,7 @@ fn example_1_puzzle_1() {
     assert_eq!(puzzle_1(self::EXAMPLE_INPUT), Ok(288));
 }
 
-// #[test]
-// fn example_1_puzzle_2() {
-//     assert_eq!(puzzle_2(self::EXAMPLE_INPUT), Ok(46));
-// }
+#[test]
+fn example_1_puzzle_2() {
+    assert_eq!(puzzle_2(self::EXAMPLE_INPUT), Ok(71503));
+}
